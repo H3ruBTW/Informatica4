c33dab33cc1e0249cb392fbc5d67cfbede5687cf
H3ruBTW <alessandro.buongallino@gmail.com>
2024-01-29 09:37:53 +0100

    html

---
+++ Informatica/4°ANNO/HTML/JavaScript/ES_02/ES_A/script.js
@@ -0,0 +1,10 @@
+let lung = prompt("Scrivi la lunghezza della scatola: ");
+let alt = prompt("Scrivi l'altezza della scatola: ");
+let lar = prompt("Scrivi la larghezza della scatola: ");
+
+let vol = lung * alt * lar;
+
+console.log("Lunghezza = " + lung);
+console.log("Altezza = " + alt);
+console.log("Larghezza = " + lar);
+console.log(vol);
