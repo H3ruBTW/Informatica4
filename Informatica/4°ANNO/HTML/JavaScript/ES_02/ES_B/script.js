f446596c0f28a77452be568c5c4f41b42a4df402
H3ruBTW <alessandro.buongallino@gmail.com>
2024-01-29 10:03:29 +0100

    codi

---
+++ Informatica/4°ANNO/HTML/JavaScript/ES_02/ES_B/script.js
@@ -0,0 +1,12 @@
+let m = prompt("y=mx+c\nScrivere la m:");
+let c = prompt("y=mx+c\nScrivere la c:");
+let x1 = -1;
+let x2 = 10;
+
+let ris1 = m * x1 + (c*1);
+let ris2 = m * x2 + (c*1);
+
+console.log("m = " + m);
+console.log("c = " + c);
+console.log("Risultato 1 con x = -1: " + ris1);
+console.log("Risultato 2 con x = 10: " + ris2);
