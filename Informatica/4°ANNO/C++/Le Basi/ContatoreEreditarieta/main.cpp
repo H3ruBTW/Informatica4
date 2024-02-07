7815c50c74f13c1d2c4ade30fc7126dadaed88f6
H3ruBTW <alessandro.buongallino@gmail.com>
2024-02-07 13:49:33 +0100

    commit

--- Informatica/4°ANNO/C++/Le Basi/ContatoreEreditarieta/main.cpp
+++ Informatica/4°ANNO/C++/Le Basi/ContatoreEreditarieta/main.cpp
@@ -4,52 +4,56 @@ using namespace std;
 
 int main()
 {
-    Contatore c1(0, 10);
-    ContatoreDoppio c2;
+     Contatore c1(0, 10);
+     ContatoreDoppio c2;
 
-    c2.setPasso(5);
+     c2.setPasso(5);
 
-    cout << "-- Pre Incremento --" << endl
-         << "Oggetto C1:" << endl
-         << "Valore: " << c1.getValore() << endl
-         << "Passo: " << c1.getPasso() << endl
-         << endl
-         << "Oggetto C2:" << endl
-         << "Valore: " << c2.getValore() << endl
-         << "Passo: " << c2.getPasso() << endl;
+     cout << "-- Pre Incremento --" << endl
+          << "Oggetto C1:" << endl
+          << "Valore: " << c1.getValore() << endl
+          << "Passo: " << c1.getPasso() << endl
+          << endl
+          << "Oggetto C2:" << endl
+          << "Valore: " << c2.getValore() << endl
+          << "Passo: " << c2.getPasso() << endl;
 
-    cout << "C1:";
-    c1.inc();
+     cout << endl
+          << "C1:";
+     c1.inc();
 
-    cout << "C2:";
-    c2.incrementaDoppio();
+     cout << endl
+          << "C2:";
+     c2.incrementaDoppio();
 
-    cout << endl
-         << "-- Post Incremento / Pre Decremento -- " << endl
-         << "Oggetto C1:" << endl
-         << "Valore: " << c1.getValore() << endl
-         << "Passo: " << c1.getPasso() << endl
-         << endl
-         << "Oggetto C2:" << endl
-         << "Valore: " << c2.getValore() << endl
-         << "Passo: " << c2.getPasso() << endl;
+     cout << endl
+          << "-- Post Incremento / Pre Decremento -- " << endl
+          << "Oggetto C1:" << endl
+          << "Valore: " << c1.getValore() << endl
+          << "Passo: " << c1.getPasso() << endl
+          << endl
+          << "Oggetto C2:" << endl
+          << "Valore: " << c2.getValore() << endl
+          << "Passo: " << c2.getPasso() << endl;
 
-    cout << "C1:";
-    c1.dec();
+     cout << endl
+          << "C1:";
+     c1.dec();
 
-    cout << "C2:";
-    c2.decrementaDoppio();
+     cout << endl
+          << "C2:";
+     c2.decrementaDoppio();
 
-    cout << endl
-         << "-- Post Decremento -- " << endl
-         << "Oggetto C1:" << endl
-         << "Valore: " << c1.getValore() << endl
-         << "Passo: " << c1.getPasso() << endl
-         << endl
-         << "Oggetto C2:" << endl
-         << "Valore: " << c2.getValore() << endl
-         << "Passo: " << c2.getPasso() << endl
-         << endl;
+     cout << endl
+          << "-- Post Decremento -- " << endl
+          << "Oggetto C1:" << endl
+          << "Valore: " << c1.getValore() << endl
+          << "Passo: " << c1.getPasso() << endl
+          << endl
+          << "Oggetto C2:" << endl
+          << "Valore: " << c2.getValore() << endl
+          << "Passo: " << c2.getPasso() << endl
+          << endl;
 
-    return 0;
+     return 0;
 }
