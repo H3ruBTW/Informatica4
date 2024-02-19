7a3168a623bbc08c67ee116f5ca79487485196a1
H3ruBTW <alessandro.buongallino@gmail.com>
2024-02-19 10:41:57 +0100

    commit

--- Informatica/4°ANNO/HTML/JavaScript/ES_03/ES_C/script.js
+++ Informatica/4°ANNO/HTML/JavaScript/ES_03/ES_C/script.js
@@ -3,87 +3,82 @@ let ris, oper;
 
 let num1 = prompt("Calcolatrice\nScrivere il primo numero");
 
-if(Number.isNaN(num1)) //controllo variabile "Not a Number"
+//ciclo per vedere se l'utente inserisce un operatore disponibile
+do
 {
-    console.error("Nel prompt è stato inserito un NaN");
-}
-else 
-{
-    //ciclo per vedere se l'utente inserisce un operatore disponibile
-    do
+    if(d==false)
     {
-        if(d==false)
-        {
-            d=true;
-        }
-        else
-        {
-            alert("Hai inserito un operatore non esistente!");
-        }
+        d=true;
+    }
+    else
+    {
+        alert("Hai inserito un operatore non esistente!");
+    }
 
-        oper = prompt("Inserire l'operatore\nOpzioni:\n+(addizione)|-(sottrazione)|*(moltiplicazione)|/(divisione)|**(potenza)");
-    
-    }while(oper!="+" && oper!="-" && oper!="*" && oper!="/" && oper!="**");
-}
+    oper = prompt("Inserire l'operatore\nOpzioni:\n+(addizione)|-(sottrazione)|*(moltiplicazione)|/(divisione)|**(potenza)");
+
+}while(oper!="+" && oper!="-" && oper!="*" && oper!="/" && oper!="**");
+
 
 let num2 = prompt("Calcolatrice\nScrivere il secondo numero");
 
-if(Number.isNaN(num2)) //controllo variabile "Not a Number"
+if(isNaN(num1+num2))
 {
-    console.error("Nel prompt è stato inserito un NaN");
+    console.error("Hai inserito un NaN");
 }
 else
 {
     switch(oper)
     {
-        case "+":
-            ris = (num1 * 1) + (num2 * 1);
-            console.log(num1 + " + " + num2 + " = " + ris);
-            break;
+    case "+":
+        ris = (num1 * 1) + (num2 * 1);
+        console.log(num1 + " + " + num2 + " = " + ris);
+        break;
 
-        case "-":
-            ris = num1 - num2;
-            console.log(num1 + " - " + num2 + " = " + ris);
-            break;
+    case "-":
+        ris = num1 - num2;
+        console.log(num1 + " - " + num2 + " = " + ris);
+        break;
 
-        case "*":
-            ris = num1 * num2;
-            console.log(num1 + " * " + num2 + " = " + ris);
-            break;
+    case "*":
+        ris = num1 * num2;
+        console.log(num1 + " * " + num2 + " = " + ris);
+        break;
 
-        case "/":
-            if(num1==0 || num2==0)
+    case "/":
+        if(num1==0 || num2==0)
+        {
+            console.error("Impossibile dividere per 0");
+        }
+        else
+        {
+            ris = num1 / num2;
+            console.log(num1 + " / " + num2 + " = " + ris);
+        }
+        break;
+
+    case "**":
+        if(num1==0 && num2==0)
+        {
+            console.log("Forma indeterminata");
+            break;
+        }
+
+        if(num1==0)
+            ris=0;
+        else
+        {
+            ris=1;
+
+            for(let i=0; i<num2; i++)
             {
-                console.error("Impossibile dividere per 0");
+                ris*=num1;
             }
-            else
-            {
-                ris = num1 / num2;
-                console.log(num1 + " / " + num2 + " = " + ris);
-            }
-            break;
+        }   
 
-        case "**":
-            if(num1==0 && num2==0)
-            {
-                console.log("Forma indeterminata");
-                break;
-            }
+        console.log(num1 + " ^ " + num2 + " = " + ris);
 
-            if(num1==0)
-                ris=0;
-            else
-            {
-                ris=1;
-
-                for(let i=0; i<num2; i++)
-                {
-                    ris*=num1;
-                }
-            }   
-
-            console.log(num1 + " ^ " + num2 + " = " + ris);
-
-            break;
+        break;
     }
+
 }
