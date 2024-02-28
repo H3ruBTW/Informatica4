11b44a4e473d8dd796b7ce5683e291323ec22769
H3ruBTW <alessandro.buongallino@gmail.com>
2024-02-28 16:12:27 +0100

    commit

--- TPSIT/4°anno/Java/Array/src/Pila/EsPila.java
+++ TPSIT/4°anno/Java/Array/src/Pila/EsPila.java
@@ -1,23 +1,86 @@
 package Pila;
+import java.util.Scanner;
 
 public class EsPila {
 
     public static void main(String[] args) {
-        try{
-            
 
-            
+        Scanner scanner = new Scanner(System.in);
+
+        try{
+            if(args.length!=1)
+            {
+                System.err.println("Non hai inserito il numero giusto di parametri da linea di comando");
+                System.err.println("Sintassi: Numero Intero(grandezza del array)");
+                System.exit(-1); 
+            }
+
+            int N = Integer.parseInt(args[0]);
+            Pila pila = new Pila(N);
+            int array[] = new int[N];
+
+            int d, n;
+
+            while ((d=menù())!=0) {
+                
+                switch (d) {
+                    case 1:
+                        System.out.print("Scrivere un numero da inserire nell'array: ");
+                        n=scanner.nextInt();
+                        pila.push(array, n);
+                        break;
+                    case 2:
+                        pila.pop(array);
+                        break;
+                    case 3:
+                        n=pila.top(array);
+                        System.out.println("L'elemento visualizzato è " + n);
+                        break;
+                    default:
+                        System.out.println("Hai inserito un numero non esistente nella lista");
+                        break;
+                }
+            }
+
+            System.out.println("Stai per chiudere il programma");
+        
+            scanner.close();
+        }   
+        catch (Exception e)
+        {
+            System.err.println("\nHai inserito un carattere non disponibile");
+            System.err.println("Sintassi: Numero Intero(grandezza del array)");
+            System.err.println("");
+            System.err.println(e);
+
+            scanner.close();
+
+            System.exit(-1);
         }
     }
 
-    private void menù(){
+    private static int menù(){
 
+        int d;
+        System.out.println("Inserire 1 per inserire l'elemento nella Pila");
+        System.out.println("Inserire 2 per eliminare l'ultimo elemento nella Pila");
+        System.out.println("Inserire 3 per vedere l'ultimo elemento nella Pila");
+        System.out.println("Inserire 0 per uscire");
+        System.out.print("Inserimento: ");
+
+        Scanner scanner2 = new Scanner(System.in);
+
+        d = scanner2.nextInt();
+
+        scanner2.close();
+        
+        return d;
     }
 }
 
 class Pila {
     private int fine=0;
-    private int N;
+    private final int N;
 
     public Pila (int N)
     {
