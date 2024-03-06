5a1ffef5d94f0e62c78b8d339db17122b706d978
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-03-06 15:57:24 +0000

    commit

---
+++ TPSIT/4°anno/Java/ES_04/ES_A.java
@@ -0,0 +1,19 @@
+package ES_04;
+
+import java.util.Scanner;
+
+public class ES_A {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] Args)
+    {
+        String s = new String[];
+
+        s = scanner.nextLine();
+
+        System.out.println("\nLa lunghezza è " + s.lenght())
+
+        
+    }
+}
