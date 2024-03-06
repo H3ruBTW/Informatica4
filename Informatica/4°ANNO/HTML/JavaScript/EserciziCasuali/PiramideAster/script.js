441aaedf282b5b682d262026a39d4dc949f27c53
H3ruBTW <alessandro.buongallino@gmail.com>
2024-03-06 12:40:41 +0100

    commit

---
+++ Informatica/4°ANNO/HTML/JavaScript/EserciziCasuali/PiramideAster/script.js
@@ -0,0 +1,11 @@
+do
+{
+    let alt = prompt("Si prega d'inserire un numero maggiore o uguale ad uno\nInserire l'altezza della piramide");
+}while(alt < 1);
+
+for(int i=0; i<alt; i++)
+{
+    
+}
+
+
