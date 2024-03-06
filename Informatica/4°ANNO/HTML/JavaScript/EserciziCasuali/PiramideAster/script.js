c6e3be67f89527024e71c55f2477b8477bd1b184
H3ruBTW <alessandro.buongallino@gmail.com>
2024-03-06 13:11:07 +0100

    commit

--- Informatica/4°ANNO/HTML/JavaScript/EserciziCasuali/PiramideAster/script.js
+++ Informatica/4°ANNO/HTML/JavaScript/EserciziCasuali/PiramideAster/script.js
@@ -1,11 +1,37 @@
+let spaziIn = "";
+let spaziMez = "";
+let asterischi = "*";
+let alt;
+
 do
 {
-    let alt = prompt("Si prega d'inserire un numero maggiore o uguale ad uno\nInserire l'altezza della piramide");
+    alt = prompt("Si prega d'inserire un numero maggiore o uguale ad uno\nInserire l'altezza della piramide");
 }while(alt < 1);
 
-for(int i=0; i<alt; i++)
+for(let i=1; i<alt; i++)
+    spaziIn = spaziIn + " ";
+
+console.log(spaziIn + "*");
+
+for(let i=1; i<alt-1; i++)
 {
-    
+    spaziIn = "";
+    spaziMez = " ";
+
+    for(let j=1; j<alt-i; j++)
+        spaziIn = spaziIn + " ";
+
+    for(let j=1; j<i; j++)
+        spaziMez = spaziMez + "  ";
+
+    console.log(spaziIn + "*" + spaziMez + "*");
 }
 
+if(alt>1)
+{
+    for(let i=1; i<alt; i++)
+        asterischi = asterischi + "**";
+}
+
+console.log(asterischi);
 
