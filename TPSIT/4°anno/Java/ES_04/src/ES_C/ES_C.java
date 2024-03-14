2b61940db8da3f1f942476cb694484de280188d4
H3ruBTW <alessandro.buongallino@gmail.com>
2024-03-14 21:22:41 +0100

    commit

--- TPSIT/4°anno/Java/ES_04/src/ES_C/ES_C.java
+++ TPSIT/4°anno/Java/ES_04/src/ES_C/ES_C.java
@@ -74,8 +74,58 @@ public class ES_C {
         {
             System.out.println("L'URL è valido");
         }
+        else
+        {
+            System.out.println("L'URL non è valido");
+        }
 
         //4
+        n=0;
+        int k=-1, j=0, d=0;
+
+        for (int i = 0; i < s.length(); i++) 
+        {
+            if(s.charAt(i)=='.')
+                n++;
+        }
+
+        if(n==3)
+        {
+            for (int i = 0; i < s.length() && d==0; i++) 
+            {
+                if(s.charAt(i)=='.')
+                {
+                    if(s.charAt(k+1)=='.')
+                    {
+                        d=1;
+                        continue;
+                    }
+
+                    k=i;
+
+                    if(Integer.parseInt(s.substring(j, k))<0 && Integer.parseInt(s.substring(j, k))>255)
+                    {
+                        d=1;
+                        continue;
+                    }
+
+                    j=k+1;
+                }
+            }
+
+            if(d==0)
+            {
+                System.out.println("L'indirizzo IP è valido");
+            }
+            else
+            {
+                System.out.println("L'indirizzo IP non è valido");
+            }
+        }
+        else
+        {
+            System.out.println("L'indirizzo IP non è valido");
+        }
         
         //5
 
