019679897f688087b9c9953ce44978c12168b669
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-04-19 11:33:29 +0200

    Add files via upload

---
+++ Informatica/4°ANNO/HTML/Normale/A/js/script.js
@@ -0,0 +1,9 @@
+// script.js
+// Esempio di JavaScript per il modulo di contatto
+
+// Codice JavaScript per la validazione del modulo di contatto
+// (puoi implementare questa parte a seconda delle tue esigenze)
+function inviaModulo() {
+    // Logica per l'invio del modulo di contatto
+    alert('Grazie! Il tuo messaggio è stato inviato.');
+}
