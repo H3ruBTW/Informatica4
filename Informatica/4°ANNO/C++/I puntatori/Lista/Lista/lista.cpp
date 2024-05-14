757bff00713538d49b2f5ad9f4669c8eae792559
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-05-14 08:03:28 +0000

    commit

--- Informatica/4°ANNO/C++/I puntatori/Lista/Lista/lista.cpp
+++ Informatica/4°ANNO/C++/I puntatori/Lista/Lista/lista.cpp
@@ -33,6 +33,57 @@ void lista::cancInTesta()
     }
 }
 
+void lista::insInCoda(int n)
+{
+    if(isEmpty())
+    {
+        L = new s_nodo;
+        L->info = n;
+        L->next = NULL:
+    }
+    else
+    {
+        s_nodo* p = L, p2 = new s_nodo;
+    }
+}
+
+void lista::cancInCoda()
+{
+    if(!isEmpty())
+    {
+        if(L->next==NULL)
+        {
+            L = L->next;
+            free(p);
+        }
+        else
+        {
+            s_nodo* p2 = L->next;
+            for(;p2->next != NULL; p2 = p2->next);
+            p->next=NULL;
+            free(p2);
+        }
+    }
+    else
+    {
+        cout << "Errore" << endl;
+    }
+}
+
+void lista::stampaLista()
+{
+    s_nodo* p;
+
+    cout << "Informazioni: "
+
+    for(p=L; p!=NULL; p = p->next)
+    {
+        cout << p->info << " ";
+    }
+
+    cout << endl;
+}
+
 bool lista::isEmpty()
 {
     if(L==NULL) return true;
