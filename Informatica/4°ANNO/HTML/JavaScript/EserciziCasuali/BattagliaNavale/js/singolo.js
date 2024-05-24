6d25c6f2daf753bd5fcb0bd094cee4f7fbdb1f6c
H3ruBTW <alessandro.buongallino@gmail.com>
2024-05-25 00:16:23 +0200

    commit

--- Informatica/4°ANNO/HTML/JavaScript/EserciziCasuali/BattagliaNavale/js/singolo.js
+++ Informatica/4°ANNO/HTML/JavaScript/EserciziCasuali/BattagliaNavale/js/singolo.js
@@ -47,7 +47,7 @@ plays.forEach(play => {
                     matrice[this.getAttribute("data-n")]=0
                     nNavi++
                 }
-                h3.innerHTML = "NUMERO DI NAVI DA INSERIRE: " + nNavi
+                aggiornaH3()
             }
             else
             {
@@ -65,25 +65,76 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let i = 0; i < 100; i++) {
         matrice[i] = plays[i].getAttribute("data-value")
         plays[i].setAttribute("data-n", i)
+        posis[i].setAttribute("data-n", i)
     }
 
-    h3.innerHTML = "NUMERO DI NAVI DA INSERIRE: " + nNavi
+    aggiornaH3()
 })
 
 let Confirm = document.getElementById("bottone")
 let tab2 = document.getElementById("tab2")
-tab2.hidden = false
+tab2.hidden = true
 
 Confirm.addEventListener("click", function(){
-    if(nNavi==0)
+    if(inizio === true)
     {
-        Confirm.hidden = true
-        tab2.hidden = false
-        inizio = false
+        if(nNavi==0)
+        {
+            tab2.hidden = false
+            inizio = false
+
+            for (let i = 0; i < 100; i++) {
+                posis[i].setAttribute("data-value", matrice[i])
+                aggiornaTh(i)
+            }
+
+            inizializzazioneBot()
+        }
+        else
+        {
+            /* FARE DIV ABSOLUTE CON AVVERTIMENTO CHE SCOMPARE DOPO POCHI SECONDI */
+            alert("HAI INSERITO TROPPE O TROPPO POCHE NAVI")
+        }
     }
     else
     {
-        /* FARE DIV ABSOLUTE CON AVVERTIMENTO CHE SCOMPARE DOPO POCHI SECONDI */
-        alert("HAI INSERITO TROPPE O TROPPO POCHE NAVI")
+
     }
 })
+
+let posis = document.querySelectorAll('#pos')
+
+function aggiornaH3(){
+    h3.innerHTML = "NUMERO DI NAVI DA INSERIRE: " + nNavi
+}
+
+function aggiornaTh(n){
+    switch(posis[n].getAttribute("data-value")){
+        case "0":
+            posis[n].innerHTML = "◎"
+            break
+        case "1":
+            posis[n].innerHTML = "◉"
+            break
+        case "2":
+            posis[n].innerHTML = "X"
+            break
+        case "3":
+            posis[n].innerHTML = "O"
+            break
+    }
+}
+
+let matriceBot = [100]
+
+matriceBot = 0
+
+function inizializzazioneBot(){
+    for(let i=0; i<5; i++)
+    {
+        //NUMERI CASUALI BOT
+        let num
+
+
+    }
+}
