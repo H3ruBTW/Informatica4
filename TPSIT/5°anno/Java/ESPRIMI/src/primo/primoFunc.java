c65206b97047a07915d64ab8cc89fc9a49722f14
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-09-18 13:38:27 +0200

    commit

--- TPSIT/5°anno/Java/ESPRIMI/src/primo/primoFunc.java
+++ TPSIT/5°anno/Java/ESPRIMI/src/primo/primoFunc.java
@@ -5,8 +5,20 @@ public class primoFunc {
         System.out.println("Ciao!");
     }
 
+    primoFunc(int x){
+        System.out.println(x + " + " + x + " = " + somma2(x,x));
+    }
+
     primoFunc(int x, int y){
         System.out.println(x + " + " + y + " = " + somma2(x,y));
+    }
+
+    primoFunc(int x, int y, int z){
+        System.out.println(x + " + " + y + " + " + z  + " = " + somma2(somma2(x,y), z));
+    }
+
+    primoFunc(int x, int y, int z, int x2){
+        System.out.println(x + " * " + y + " * " + z + " + " + x2 + " = " + somma2(prod3(x, y, z), x2));
     }
 
     int somma2 (int x, int y){
--- TPSIT/5°anno/Java/ESPRIMI/src/primo/primomain.java
+++ TPSIT/5°anno/Java/ESPRIMI/src/primo/primomain.java
@@ -3,7 +3,11 @@ package primo;
 public class primoMain {
     public static void main(String[] args) {
         primoFunc primo = new primoFunc();
-        primoFunc primo2 = new primoFunc(15, 5);
+        primoFunc primo2 = new primoFunc(5);
+        primoFunc primo3 = new primoFunc(5,15);
+        primoFunc primo4 = new primoFunc(5,7,9);
+        primoFunc primo5 = new primoFunc(2,2,2,2);
+
         System.out.println("Il risultato è " + primo.somma2(3, 5));
         System.out.println("Il risultato è " + primo2.prod3(2, 2, 3));
     }
