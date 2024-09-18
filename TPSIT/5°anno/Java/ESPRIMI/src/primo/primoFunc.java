8db220b96942b0aa57f7c014e2dc954a72e48c1d
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-09-18 13:01:29 +0200

    commit

---
+++ TPSIT/5°anno/Java/ESPRIMI/src/primo/primoFunc.java
@@ -0,0 +1,11 @@
+package primo;
+
+public class primoFunc {
+    int somma2 (int x, int y){
+        return x+y;
+    }
+
+    int prod3 (int x, int y, int z){
+        return x*y*z;
+    }
+}
--- TPSIT/5°anno/Java/ESPRIMI/src/primo/primomain.java
+++ TPSIT/5°anno/Java/ESPRIMI/src/primo/primomain.java
@@ -1,7 +1,9 @@
 package primo;
 
-public class primomain {
+public class primoMain {
     public static void main(String[] args) {
-        
+        primoFunc primo = new primoFunc();
+        System.out.println("Il risultato è " + primo.somma2(3, 5));
+        System.out.println("Il risultato è " + primo.prod3(2, 2, 3));
     }
 }
