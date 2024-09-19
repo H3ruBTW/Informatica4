e1f168467339e63d02b610d5ab3750083ab610ac
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-09-19 15:07:01 +0200

    commit

---
+++ Informatica/5°ANNO/PHP/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ES A alla F - HomePage</title>
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="img/icon.png">
+</head>
+<body>
+    <header>
+        <h1>NAVIGAZIONE ESERCIZI</h1>
+        <nav>    
+            <ul>
+                <li><a href="index.php">HomePage</a></li>
+                <li><a href="ES_01/index.php">ES_01 - BASI PHP</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <footer>
+        <p>&copy; 2024-2025 Buongallino Alessandro</p>
+    </footer>
+</body>
+</html>
