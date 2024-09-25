604f55a8c9baf64b2e5fcb8d8076bd75d0ed37df
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-09-25 13:11:33 +0200

    commit

--- TPSIT/5°anno/Java/GregorianCalendar/src/CalendiarioGregoriano/CalendarioMetodi1.java
+++ TPSIT/5°anno/Java/GregorianCalendar/src/CalendiarioGregoriano/CalendarioMetodi1.java
@@ -5,12 +5,14 @@ import java.util.*;
 public class CalendarioMetodi1 {
     void Orario(){
         GregorianCalendar Cal = new GregorianCalendar();
+
         int giorno = Cal.get(Calendar.DAY_OF_MONTH);
         int mese = Cal.get(Calendar.MONTH)+1;
         int anno = Cal.get(Calendar.YEAR);
         int H = Cal.get(Calendar.HOUR_OF_DAY);
         int m = Cal.get(Calendar.MINUTE);
         int S = Cal.get(Calendar.SECOND);
+        
         System.out.println("Orario di avvio metodo prima Classe:");
         System.out.println(giorno + "/" + mese + "/" + anno + " - " + H + ":" + m + ":" + S);
     }
