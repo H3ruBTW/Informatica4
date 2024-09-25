c329a4d2172bdb0212c16f8203598b85e8613893
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-09-25 13:11:22 +0200

    commit

--- TPSIT/5°anno/Java/GregorianCalendar/src/CalendiarioGregoriano/CalendarioMetodi2.java
+++ TPSIT/5°anno/Java/GregorianCalendar/src/CalendiarioGregoriano/CalendarioMetodi2.java
@@ -7,6 +7,7 @@ public class CalendarioMetodi2 {
     void Orario(){
         GregorianCalendar Cal = new GregorianCalendar();
         SimpleDateFormat SDF = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
+        
         System.out.println("Orario di avvio metodo seconda Classe:");
         System.out.println(SDF.format(Cal.getTime()));
     }
