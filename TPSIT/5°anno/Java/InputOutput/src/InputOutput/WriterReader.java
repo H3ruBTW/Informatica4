d0d5e10f4dd2caa4dd39e216981e2a1191d4573c
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-10-02 15:18:17 +0200

    commit

--- TPSIT/5°anno/Java/InputOutput/src/InputOutput/WriterReader.java
+++ TPSIT/5°anno/Java/InputOutput/src/InputOutput/WriterReader.java
@@ -6,10 +6,21 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-public class ReadAndWrite{
+public class WriterReader{
     public static void main() throws IOException{
         BufferedReader legge = new BufferedReader(new FileReader("/mnt/c/Users/aless/Desktop/Codice/Informatica4/TPSIT/5°anno/Java/InputOutput/src/InputOutput/testo/lettura.txt"));
         BufferedWriter scrive = new BufferedWriter(new FileWriter("/mnt/c/Users/aless/Desktop/Codice/Informatica4/TPSIT/5°anno/Java/InputOutput/src/InputOutput/testo/lettura.txt"));
 
+        int next;
+
+        do{
+            next = legge.read();
+            if (next != -1) {
+                scrive.write(next);
+            }
+        }while(next != -1);
+
+        legge.close();
+        scrive.close();
     }
 }
