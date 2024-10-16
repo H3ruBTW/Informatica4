9d4d0853ec1c61e404342c7bb5cef295ff5004ea
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-10-16 12:47:23 +0200

    commit

--- TPSIT/5°anno/Java/Ereditarieta/src/Cerchio/Principale.java
+++ TPSIT/5°anno/Java/Ereditarieta/src/Cerchio/Principale.java
@@ -11,19 +11,18 @@ public class Principale {
             raggio = scanner.nextFloat();
             System.out.print("Scrivere l'altezza: ");
             altezza = scanner.nextFloat();
-        }
-
-        AreaCerchio ObjCerchio = new AreaCerchio(raggio);
-        VolumeCilindro ObjCilindro = new VolumeCilindro(raggio, altezza);
-        VolumeSfera ObjSfera = new VolumeSfera(raggio);
-
-        try {
+            
+            AreaCerchio ObjCerchio = new AreaCerchio(raggio);
+	        VolumeCilindro ObjCilindro = new VolumeCilindro(raggio, altezza);
+	        VolumeSfera ObjSfera = new VolumeSfera(raggio);
+	
             System.out.println("\nCalcolo...");
             System.out.println("\nArea del cerchio:    " + ObjCerchio.Area(raggio));
             System.out.println("Volume del Cilindro: " + ObjCilindro.Volume(raggio, altezza));
             System.out.println("Volume della sfera:  " + ObjSfera.Volume(raggio));
+	        
         } catch (Exception e) {
-            System.err.println("Errore nel calcolo...\n" + e);
-        }
+        	System.out.println("\nIl programma è andato in errore");
+        }  
     }
 }
