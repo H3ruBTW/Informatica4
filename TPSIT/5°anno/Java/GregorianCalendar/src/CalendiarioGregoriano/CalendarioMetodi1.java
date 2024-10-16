d7baa6dc3f3b5ef221c7a2efc12050e44f824938
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-10-16 12:39:41 +0200

    commit

--- TPSIT/5°anno/Java/GregorianCalendar/src/CalendiarioGregoriano/CalendarioMetodi1.java
+++ TPSIT/5°anno/Java/GregorianCalendar/src/CalendiarioGregoriano/CalendarioMetodi1.java
@@ -7,7 +7,7 @@ public class CalendarioMetodi1 {
         GregorianCalendar Cal = new GregorianCalendar();
 
         int giorno = Cal.get(Calendar.DAY_OF_MONTH);
-        int mese = Cal.get(Calendar.MONTH)+1;
+        int mese = Cal.get(Calendar.MONTH)+1; //Perchè gennaio è uguale a 0
         int anno = Cal.get(Calendar.YEAR);
         int H = Cal.get(Calendar.HOUR_OF_DAY);
         int m = Cal.get(Calendar.MINUTE);
