e860e8ac34dc0cc26c80d9845e49c60daf07b840
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-10-21 20:54:16 +0200

    commit

--- Informatica/5°anno/PHP/ES_03/js/script.js
+++ Informatica/5°anno/PHP/ES_03/js/script.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('form');
-    const submitButton = document.getElementById('button'); 
+    const submit = document.getElementById('button'); 
     const inputs = form.querySelectorAll('input[pattern], input[required]');
 
-    // Funzione per controllare la validità dei campi
     function valida() {
         inputs.forEach(input => {
             if (input.hasAttribute('required') && input.value.trim() === "") {
@@ -11,15 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
             } else if (!input.checkValidity()) {
                 input.setAttribute('class', 'rosso'); 
             } else {
-                if (input.classList.contains('rosso')) {
-                    input.setAttribute('class', ''); 
-                }
+                input.setAttribute('class', '');
             }
         });
     }
 
-    // Gestore dell'evento click sul pulsante di submit
-    submitButton.addEventListener('click', function(event) {
+    submit.addEventListener('click', function(event) {
         valida();
     });
 });
