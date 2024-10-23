23a7866b485bf4ef6bd7d39a6bdba55a2d05f124
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-10-23 13:35:06 +0200

    commit

--- TPSIT/5°anno/Java/Threads/src/ES_02/Main.java
+++ TPSIT/5°anno/Java/Threads/src/ES_02/Main.java
@@ -15,8 +15,10 @@ public class Main {
         Piramide piramide = new Piramide(lato, altezza);
         
         System.out.println("\nArea della base della piramide: " + piramide.areaQuadrato()); 
+
         System.out.println("\nArea della faccia triangolare: " + piramide.areaTriangolo()); 
         System.out.println("Perimetro della faccia triangolare: " + piramide.perimetroTriangolo()); 
+        
         System.out.println("\nArea superficiale della piramide: " + piramide.superficiePiramide()); 
         System.out.println("Volume della piramide: " + piramide.volumePiramide());
 
