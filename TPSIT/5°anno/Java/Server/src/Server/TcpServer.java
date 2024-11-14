d30e2fc441d7b5778627e5c477bcc9919819bfce
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2024-11-14 15:05:21 +0100

    commit

--- TPSIT/5°anno/Java/Server/src/Server/TcpServer.java
+++ TPSIT/5°anno/Java/Server/src/Server/TcpServer.java
@@ -31,21 +31,48 @@ public class TcpServer {
 				// Create output stream to write data and input stream to read data from socket
 				DataOutputStream outStream = new DataOutputStream(clientSocket.getOutputStream());	
 				BufferedReader inStream = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-	
+				
+				String substring;
+				int cond = -1;
+
 				// ---------------------------------------------------------
 				//Lettura dati dal client un righa alla volta   
 				while ((clientMsg=inStream.readLine()).length() != 0) {
 					System.out.println(clientMsg);	
-				}  
+					if(clientMsg.contains("GET") || clientMsg.contains("POST")){
+						
+						if(clientMsg.indexOf("HTTP/1.1")-2 - clientMsg.indexOf("/")+1 > 0){
+
+							substring = clientMsg.substring(clientMsg.indexOf("/")+1, clientMsg.indexOf("HTTP/1.1")-1);
+
+							if(substring.toLowerCase().compareTo("accendi") == 0){
+								cond = 1;
+							} else if (substring.toLowerCase().compareTo("spegni") == 0){
+								cond = 0;
+							}
+						}
+					}
+				} 
+
 				// Elaborare qui i dati ricevuti dal client 
 				// ---------------------------------------------------------
-
 				//Invio dei dati su stream di rete al client
 				clientMsg = "HTTP/1.1 200 OK\r\n";
 				//clientMsg += "Connection: close\r\n";
 				//clientMsg += "Content-Type: text/plain\r\n";
 				clientMsg += "\r\n";
-				clientMsg += "Buongallino Alessandro";
+				switch (cond) {
+					case 1:
+						clientMsg += "ACCESO";
+						break;
+					case 0:
+						clientMsg += "SPENTO";
+						break;
+					default:
+						clientMsg += "CONDIZIONE NON ESISTENTE O NON ASSEGNATA";
+						break;
+				}
+				
 				outStream.write(clientMsg.getBytes());
 				outStream.flush();
 
