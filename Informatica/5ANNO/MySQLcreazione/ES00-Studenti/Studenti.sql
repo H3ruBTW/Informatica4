036f5be1fe0e56b74af96ad20b64bbf5899f2a51
H3ruBTW <75161843+H3ruBTW@users.noreply.github.com>
2025-01-16 23:28:53 +0100

    commit

--- Informatica/5ANNO/MySQLcreazione/ES00-Studenti/Studenti.sql
+++ Informatica/5ANNO/MySQLcreazione/ES00-Studenti/Studenti.sql
@@ -33,32 +33,28 @@ CREATE TABLE Valutazioni (
 );
 
 -- Inserimento dei dati
-INSERT INTO Corsi (nome_corso) values (
-    "Informatica",
-    "Artistico",
-    "Telecomunicazioni"
-);
+INSERT INTO Corsi (nome_corso) values 
+    ("Informatica"),
+    ("Artistico"),
+    ("Telecomunicazioni");
 
-INSERT INTO Materie (nome_materia) values (
-    "Matematica",
-    "Italiano",
-    "Informatica"
-);
+INSERT INTO Materie (nome_materia) values 
+    ("Matematica"),
+    ("Italiano"),
+    ("Informatica");
 
-INSERT INTO Studenti (cognome, data_nascita, codice_corso, capogruppo) values (
-    "Angelico", "2006-01-01", 1, 1,
-    "Baglì", "2006-02-02", 1, 1,
-    "Buongallino", "2006-03-03", 1, 2,
-    "Spinola", "2006-04-04", 1, 2,
-);
+INSERT INTO Studenti (cognome, data_nascita, codice_corso, capogruppo) values 
+    ("Angelico", "2006-01-01", 1, 1),
+    ("Baglì", "2006-02-02", 1, 1),
+    ("Buongallino", "2006-03-03", 1, 2),
+    ("Spinola", "2006-04-04", 1, 2);
 
-INSERT INTO Valutazioni (matricola, codice_materia, voto, commento) values (
-    1, 1, 8, "COMPITO SULLE FUNZIONI",
-    1, 2, 7, "VERGA",
-    2, 1, 6, "COMPITO SULLE FUNZIONI",
-    2, 2, 8, "VERGA",
-    3, 1, 9, "COMPITO SULLE FUNZIONI",
-    3, 2, 7, "VERGA",
-    4, 1, 4, "COMPITO SULLE FUNZIONI",
-    4, 2, 6, "VERGA",
-);
+INSERT INTO Valutazioni (matricola, codice_materia, voto, commento) values 
+    (1, 1, 8, "COMPITO SULLE FUNZIONI"),
+    (1, 2, 7, "VERGA"),
+    (2, 1, 6, "COMPITO SULLE FUNZIONI"),
+    (2, 2, 8, "VERGA"),
+    (3, 1, 9, "COMPITO SULLE FUNZIONI"),
+    (3, 2, 7, "VERGA"),
+    (4, 1, 4, "COMPITO SULLE FUNZIONI"),
+    (4, 2, 6, "VERGA");
